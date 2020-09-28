1df140ad6d2cdd4328b00466f72f9e4b25bc28c6
Ashley Allen <mail@ashallendesign.co.uk>
2020-09-28 19:37:29 +0100

    Initial commit.

---
+++ src/App/Providers/ConfigValidatorProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AshAllenDesign\ConfigValidator\App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ConfigValidatorProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+    }
+}
