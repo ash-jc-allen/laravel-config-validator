a1f046b420833a7beef1fb3725aa1f1a2dbcb40e
Ashley Allen <mail@ashallendesign.co.uk>
2020-10-19 23:29:32 +0100

    Updated the test.

--- tests/Unit/Console/Commands/ValidationMakeCommand/ValidationMakeCommandTest.php
+++ tests/Unit/Console/Commands/ValidationMakeCommand/ValidationMakeCommandTest.php
@@ -8,17 +8,21 @@ use Illuminate\Support\Facades\File;
 
 class ValidationMakeCommandTest extends TestCase
 {
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        File::deleteDirectory(config_path('validation'));
+    }
+
     /** @test */
-    public function executor_can_be_created_with_the_command()
+    public function validation_file_can_be_created()
     {
         $validationFile = config_path('validation/app.php');
 
-        // Delete the executor and it's command if it exists
-        // so that we can be sure we have a new slate for
-        // testing.
-        if (File::exists($validationFile)) {
-            unlink($validationFile);
-        }
+        // Delete the validation folder so that we can be
+        // sure we have a new slate for testing.
+        File::deleteDirectory(config_path('validation'));
 
         $this->assertFalse(File::exists($validationFile));
 
@@ -39,5 +43,12 @@ return [
     Rule::make('')->rules([]),
 ];
 ";
+    }
+
+    protected function tearDown(): void
+    {
+        File::deleteDirectory(config_path('validation'));
+
+        parent::tearDown();
     }
 }
